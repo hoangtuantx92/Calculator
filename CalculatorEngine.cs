f0f9d0c570291c87ff65cd1944da3d0ed3c447bc
Kevin DO <hoangtuantx92@yahoo.com>
2019-03-16 12:50:00 -0500

    first commit

---
+++ CalculatorEngine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class CalculatorEngine
+    {
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result;
+            switch (argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                case "subtract":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "devide":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                case "multiply":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                default:
+                    throw new InvalidOperationException("input incorrect simple operator");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ InputConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class InputConverter
+    {
+        public double ConvertInputToNumberic(string argTextInput)
+        {
+            double convertedNumber;
+            if (!double.TryParse(argTextInput, out convertedNumber))
+                throw new ArgumentException("Expected a numeric value.");
+            
+            return convertedNumber;
+        }
+    }
+}
